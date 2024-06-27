1b0dc4cb4676c0a261db4a7d3750354139103cda
Matyas Szabo <forianszm@gmail.com>
2024-06-27 11:33:58 +0200

    repro

--- test.js
+++ test.js
@@ -1,23 +1,17 @@
 import { FocusRegionManager } from "@instructure/ui-a11y-utils";
 
 export function createFocusTrap(target) {
-  FocusRegionManager.activateRegion(target, {
+  const focusRegion= FocusRegionManager.activateRegion(target, {
     shouldFocusOnOpen: true,
     shouldContainFocus: true,
     shouldReturnFocus: true,
     shouldCloseOnDocumentClick: false,
     shouldCloseOnEscape: false,
   });
-
-  const removeFocusTrap = () => {
-    FocusRegionManager.blurRegion(target);
-  };
-
-  return removeFocusTrap;
+  return focusRegion;
+  //const removeFocusTrap = () => {
+  //  FocusRegionManager.blurRegion(target);
+  //};
+  //return removeFocusTrap;
 }
 
-const expertActionBarContent = document.getElementById("main");
-if (expertActionBarContent) {
-  console.log(expertActionBarContent);
-  createFocusTrap(expertActionBarContent);
-}
